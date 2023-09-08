51c8d750cf00f61591039f17e59732f9d3a820d6
Atonix0 <116169309+Atonix0@users.noreply.github.com>
2023-09-08 22:19:25 +0000

    switched to pypycord

--- src/main.py
+++ src/main.py
@@ -2,20 +2,37 @@ import discord
 # this is where my token is!!!
 import secerts
 
+bot = discord.Bot()
 
-class Main(discord.Client):
-    async def on_ready(self): 
-        print(f"logged in as {self.user}")
-    async def on_message(self, message):
-        if message.author.id == self.user.id:
-            return
-        if message.content.lower().startswith("hello"):
-            message.reply("Hello!", mention_author=True)
+@bot.event
+async def on_ready(): 
+     print(f"logged in as {bot.user}")
 
-intents = discord.Intents.default()
-intents.message_content = True
+# slash commands
+@bot.slash_command(name = "ping", description = "Ping!")
+async def ping(ctx):
+    await ctx.respond("Pong!")
 
-Client = Main(intents=intents)
+@bot.slash_command(name = "bred", description = "shows you the bred")
+async def bred(ctx):
+    await ctx.respond("bred")
 
+'''async def on_message(self, message):
+    text = message.content.lower()
+     if message.author.id == self.user.id:
+         return
+        
+    print(f"got msg = {message.content}, by {message.author}")
 
-Client.run(secerts.token)
+     # says hello to you!
+    for hello in self.hellos: 
+          if text[:len(hello)] == hello:
+             await message.reply("Hi!", mention_author=False)
+             return'''
+
+#intents = discord.Intents.default()
+#intents.message_content = True
+
+#Client = Main(intents=intents)
+
+bot.run(secerts.token)
