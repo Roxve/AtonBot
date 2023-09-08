dde860f96d5416bb10f7f1c50d54020168d5a369
Atonix0 <116169309+Atonix0@users.noreply.github.com>
2023-09-08 19:08:37 +0000

    bot

--- src/main.py
+++ src/main.py
@@ -1,2 +1,21 @@
 import discord
-exec("./secerts/main.py"
+# this is where my token is!!!
+import secerts
+
+
+class Main(discord.Client):
+    async def on_ready(self): 
+        print(f"logged in as {self.user}")
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+        if message.content.lower().startswith("hello"):
+            message.reply("Hello!", mention_author=True)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+Client = Main(intents=intents)
+
+
+Client.run(secerts.token)
